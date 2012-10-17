1d8c961ac333bb8921902ce7fd96baf4da4649a6
benyjaykay <benyjaykay@gmail.com>
2012-10-17 16:27:04 -0400

    I am doing a fresh commit from my home computer

--- test/kaplan/clock/ClockTest.java
+++ test/kaplan/clock/ClockTest.java
@@ -7,17 +7,17 @@ public class ClockTest extends TestCase {
 	public void testClock() throws InterruptedException {
 		Clocker clock = new Clocker(3, 52);
 		System.out.println(clock.getAngle());
-		assertEquals(164.0, clock.getAngle());
+		assertEquals(164.0, clock.getAngle(),.1);
 	}
 
 	public void testThreeThirty() throws InterruptedException {
 		Clocker clock = new Clocker(3, 30);
 		System.out.println(clock.getAngle());
-		assertEquals(75.0, clock.getAngle());
+		assertEquals(75.0, clock.getAngle(),.1);
 	}
 	public void testThreeOclock(){
 		Clocker clock = new Clocker(3, 00);
 		System.out.println(clock.getAngle());
-		assertEquals(270.0, clock.getAngle());
+		assertEquals(270.0, clock.getAngle(),.1);
 	}
 }
