f4c4d4c0d3a4ba0ef9fdefdc70c152ad7ec2236d
benyjaykay <benyjaykay@gmail.com>
2012-10-24 17:57:54 -0400

    All 3 classes sequential, binary, hash.

--- src/kaplan/anagram/DictionaryLoader.java
+++ src/kaplan/anagram/DictionaryLoader.java
@@ -2,31 +2,57 @@ package kaplan.anagram;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
-import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.FileUtils;
 
 public class DictionaryLoader {
-	private Scanner sc;
-	private ArrayList<String> arrDictList;
-	private String arrDictArray[];
+	private List<String> arrDictList = new ArrayList<String>();
+	private Object[] arrDictArray;
+	private File file;
 	public DictionaryLoader() throws FileNotFoundException {
-		sc = new Scanner(new File("word_list_moby_crossword_flat.txt"));
-		arrDictList = new ArrayList<String>();
-		IOUtils.readLines(input);
-		while (sc.hasNext()) {
-			arrDictList.add(sc.next());
+	 file = new File("word_list_moby_crossword_flat.txt");
+		try {
+			arrDictList = FileUtils.readLines(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		arrDictArray = new String[arrDictList.size()];
-		arrDictArray = (String[]) arrDictList.toArray();
+		arrDictArray = arrDictList.toArray();
 	}
 
 	public boolean contains(String word) {
 		return arrDictList.contains(word);
 	}
 	public boolean containsBinary(String word){
-		return Arrays.binarySearch(arrDictArray, key);
+		 if(Arrays.binarySearch(arrDictArray, word)>=0)
+			 return true;
+		 else
+			 return false;
+	}
+	public boolean containsHash(String word){
+		HashMap hm = new HashMap(); 
+		// Put elements to the map 
+		Scanner scan;
+		try {
+			scan = new Scanner(file);
+			while (scan.hasNextLine())
+	        {
+	            String line = scan.nextLine();
+	            hm.put(line,line);
+	        }
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+		if(hm.containsValue(word))return true;
+		return false;
 	}
 }
--- test/kaplan/anagram/DictLoaderTest.java
+++ test/kaplan/anagram/DictLoaderTest.java
@@ -4,9 +4,19 @@ import java.io.FileNotFoundException;
 import junit.framework.TestCase;
 
 public class DictLoaderTest extends TestCase {
-	public void testDictLoader() throws FileNotFoundException {
+	public void testSequentialDictSearch() throws FileNotFoundException {
 		DictionaryLoader dictLoader = new DictionaryLoader();
 		assertTrue(dictLoader.contains("word"));
 		assertFalse(dictLoader.contains("zzomfmfmf"));
+	}
+	public void testBinaryDictSearch()throws FileNotFoundException{
+		DictionaryLoader dictLoader = new DictionaryLoader();
+		assertTrue(dictLoader.containsBinary("word"));
+		assertFalse(dictLoader.containsBinary("zzomfmfmf"));
+	}
+	public void testHashMapSearch() throws FileNotFoundException{
+		DictionaryLoader dictLoader = new DictionaryLoader();
+		assertTrue(dictLoader.containsBinary("word"));
+		assertFalse(dictLoader.containsBinary("zzomfmfmf"));
 	}
 }
